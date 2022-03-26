a28b3ec44b65209a6caaa0fce13b0d6dd1710bb1
Tim Kinsman <tkinsm@gmail.com>
2022-03-26 12:19:24 +1030

    majims

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,9 @@
 import styles from '../styles/NotFound.module.scss';
+import maji from '../assets/maji.jpg';
 
 export const NotFound = (): JSX.Element => (
   <div className={`${styles['container']} global-fadein`}>
-    <div style={{ fontSize: '3em' }}>Page Not Found</div>
+    <img src={maji} />
+    <div style={{ fontSize: '80px' }}>square toe like majima</div>
   </div>
 );
