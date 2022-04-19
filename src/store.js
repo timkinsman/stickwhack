d0611766386e9a1b5525aed4b36bcee4034c241c
timkinsman <tkinsm@gmail.com>
2022-04-19 20:02:35 +0930

    redux logger

--- src/store.js
+++ src/store.js
@@ -1,9 +1,15 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 
+const logger = createLogger({
+  collapsed: true,
+  diff: true,
+});
+
 export default () => {
-  const middleware = [thunk];
+  const middleware = [thunk, logger];
   const store = createStore(rootReducer, applyMiddleware(...middleware));
   return { store };
 };
